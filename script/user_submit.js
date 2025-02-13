d9c82a7bbaa53f36ee1877197cea6ca19761f40c
dev_ <jhondavefuentes9@gmail.com>
2025-02-13 18:46:35 +0800

    Add files via upload

--- script/user_submit.js
+++ script/user_submit.js
@@ -1,1 +1,24 @@
 
+        document.getElementById('contactForm').addEventListener('submit', function(event) {
+            event.preventDefault(); // Prevent the default form submission
+
+            // Gather form data
+            const name = document.getElementById('yourName').value;
+            const email = document.getElementById('yourEmail').value;
+            const phone = document.getElementById('yourPhone').value;
+            const message = document.getElementById('yourMessage').value;
+
+            // Send data to EmailJS
+            emailjs.send("service_nurznhb", "template_kkq3g91", {
+                name: name,
+                email: email,
+                phone: phone,
+                message: message
+            })
+            .then(function(response) {
+                alert('Successfully submitted!');
+                document.getElementById('contactForm').reset(); // Reset the form
+            }, function(error) {
+                alert('Failed to submit: ' + JSON.stringify(error));
+            });
+        });
